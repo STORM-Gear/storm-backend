6f27c33389e2e230ccd93b243132c6e2b232e879
GaspardCulis <gaspard@gasdev.fr>
2026-06-16 14:55:10 +0200

    feat(discord): Add more memes

--- src/services/discord.rs
+++ src/services/discord.rs
@@ -3,7 +3,7 @@ use serde_json::json;
 
 use crate::{stripe::PaymentInfo, utils::get_env_var};
 
-const MEME_URLS: [&str; 4] = [
+const MEME_URLS: [&str; 8] = [
     // Stonks
     "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd214Z3p5bm1ycDVidjJzMTJidTUzYnc0MHFybWR0ZHJjeWJ6OWx4dSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/YnkMcHgNIMW4Yfmjxr/giphy.gif",
     // Cat vibe stocks
@@ -12,6 +12,14 @@ const MEME_URLS: [&str; 4] = [
     "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd214Z3p5bm1ycDVidjJzMTJidTUzYnc0MHFybWR0ZHJjeWJ6OWx4dSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/JtBZm3Getg3dqxK0zP/giphy.gif",
     // Checking daily revenue Wall Street wolf
     "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3dGN5bGFuNGpsaGtzaGFpdWFodzVsa3Y3b2pubWpzc2YzYWhkOTdqbiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/w6NLkHuoWlcdXIHktr/giphy.gif",
+    // Breaking bad money pile
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTZ2OGs4aW1lMGI3Y3gzdWw0cXNrNHZ3a3Zhb2xzdGU4MWF6a25iMiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3oKIPm3BynUpUysTHW/giphy.gif",
+    // Vince McMahon
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTZ2OGs4aW1lMGI3Y3gzdWw0cXNrNHZ3a3Zhb2xzdGU4MWF6a25iMiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/kj41Ti8GLVs1STX0bH/giphy.gif",
+    // Wall Street Wolf dropping money
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTZ2OGs4aW1lMGI3Y3gzdWw0cXNrNHZ3a3Zhb2xzdGU4MWF6a25iMiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/gTURHJs4e2Ies/giphy.gif",
+    // Donald Duck counting bills
+    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2dwa2Q1cTkxajdxd2sycjh3bTNmaHZlNHhsb2IzYXg4bGpmMjF0cyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/xTiTnqUxyWbsAXq7Ju/giphy.gif",
 ];
 
 pub struct DiscordWebhook {
