1c8297ec5692e0d616574f7fd765bd0389d3092f
GaspardCulis <gaspard@gasdev.fr>
2026-06-16 10:55:48 +0200

    chore(utils): Cleaner code

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,3 @@
 pub fn get_env_var(name: &'static str) -> String {
-    std::env::var(name).expect(format!("'{name}' env var required").as_str())
+    std::env::var(name).unwrap_or_else(|_| panic!("'{name}' env var required"))
 }
