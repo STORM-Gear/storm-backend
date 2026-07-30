ce61c2c9d285758f5faaef06593cee7b8bdde027
GaspardCulis <gaspard@gasdev.fr>
2026-07-30 14:33:02 +0200

    fix(migrations): Perform manual surgery on migration n2 to make it apply

--- toasty/migrations/0002_migration.sql
+++ toasty/migrations/0002_migration.sql
@@ -1,5 +1,16 @@
 CREATE TYPE "order_origin" AS ENUM ('stripe', 'leboncoin', 'vinted');
 CREATE TYPE "order_status" AS ENUM ('ordered', 'shipped', 'received');
-ALTER TABLE "orders" ALTER COLUMN "status" TYPE order_status;
-ALTER TABLE "orders" ADD COLUMN "origin" order_origin NOT NULL;
+
+ALTER TABLE "orders"
+  ALTER COLUMN "status" TYPE order_status
+  USING (
+    CASE status
+      WHEN 1 THEN 'ordered'
+      WHEN 2 THEN 'shipped'   -- old "Packaged" merged into shipped
+      WHEN 3 THEN 'shipped'
+      WHEN 4 THEN 'received'
+    END
+  )::order_status;
+
+ALTER TABLE "orders" ADD COLUMN "origin" order_origin NOT NULL DEFAULT 'stripe';
 ALTER TABLE "orders" ADD COLUMN "shipping" BOOLEAN;
